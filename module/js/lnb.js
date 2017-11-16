7995fa485cb3bf031477846ee815bb35c72d2e52
An-gun <msminsu@naver.com>
2017-11-16 18:03:56 +0900

    lnb 테스트

---
+++ module/js/lnb.js
@@ -0,0 +1,63 @@
+var pageMain = (function(){
+
+    var $target;
+
+    var init = function($select){
+        $target  = $($select);
+        eventBind();
+    };
+
+    var eventBind = function(){
+
+        $(document).on('click','.depth1>li>a',function(e){
+
+            e.preventDefault();
+            if($(this).parent('li').hasClass('open')){
+                $(this).closest('li').removeClass('open');
+
+            }else{
+
+                if($(this).next('ul').hasClass('depth2')){
+                    console.log();
+                    $(this).closest('li').addClass('open');
+                }
+                else {
+                    console.log('no');
+                    return;
+                }
+            }
+
+        });
+
+        $(document).on('click','.depth2>li>a',function(e){
+            e.preventDefault();
+            if($(this).closest('li').hasClass('open')){
+                $(this).closest('li').removeClass('open');
+
+            }else{
+
+                if($(this).next('ul').hasClass('depth3')){
+                    console.log();
+                    $(this).closest('li').addClass('open');
+                }
+                else {
+                    console.log('no');
+                    return;
+                }
+            }
+
+        });
+    };
+
+    var checkClass = function(_target){
+        var is = _target.hasClass('open');
+    };
+
+    return {init:init}
+
+})();
+
+
+pageMain.init('#lnb');
+
+
